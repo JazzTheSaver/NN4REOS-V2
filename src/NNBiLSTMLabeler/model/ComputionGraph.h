f5e33e750dde6aa6ee4be15c9aec890da6378e71
YuNan <yunan.hlju@gmail.com>
2017-04-18 21:17:00 +0800

    birnn

---
+++ src/NNBiLSTMLabeler/model/ComputionGraph.h
@@ -0,0 +1,107 @@
+#ifndef SRC_ComputionGraph_H_
+#define SRC_ComputionGraph_H_
+
+#include "ModelParams.h"
+
+
+// Each model consists of two parts, building neural graph and defining output losses.
+struct ComputionGraph : Graph{
+public:
+	const static int max_sentence_length = 2048;
+
+public:
+	// node instances
+	vector<LookupNode> _word_inputs;
+	WindowBuilder _word_window;
+	LSTMBuilder _left_rnn;
+	LSTMBuilder _right_rnn;
+	vector<ConcatNode> _rnn_concat;
+
+	AvgPoolNode _avg_pooling;
+	MaxPoolNode _max_pooling;
+	MinPoolNode _min_pooling;
+
+	ConcatNode _concat;
+	LinearNode _output;
+public:
+	ComputionGraph() : Graph(){
+	}
+
+	~ComputionGraph(){
+		clear();
+	}
+
+public:
+	//allocate enough nodes 
+	inline void createNodes(int sent_length){
+		_word_inputs.resize(sent_length);
+		_word_window.resize(sent_length);
+		_left_rnn.resize(sent_length);
+		_right_rnn.resize(sent_length);
+		_rnn_concat.resize(sent_length);
+		_avg_pooling.setParam(sent_length);
+		_max_pooling.setParam(sent_length);
+		_min_pooling.setParam(sent_length);
+	}
+
+	inline void clear(){
+		Graph::clear();
+		_word_inputs.clear();
+		_word_window.clear();
+		_left_rnn.clear();
+		_right_rnn.clear();
+		_rnn_concat.clear();
+	}
+
+public:
+	inline void initial(ModelParams& model, HyperParams& opts, AlignedMemoryPool* mem = NULL){
+		for (int idx = 0; idx < _word_inputs.size(); idx++) {
+			_word_inputs[idx].setParam(&model.words);
+			_word_inputs[idx].init(opts.wordDim, opts.dropProb, mem);
+		}
+		_word_window.init(opts.wordDim, opts.wordContext, mem);
+		_left_rnn.init(&model.left_rnn_params, opts.dropProb, true, mem);
+		_right_rnn.init(&model.right_rnn_params, opts.dropProb, true, mem);
+		for (int idx = 0; idx < _rnn_concat.size(); idx++)
+			_rnn_concat[idx].init(opts.hiddenSize * 2, opts.dropProb, mem);
+		_avg_pooling.init(opts.hiddenSize * 2, -1, mem);
+		_max_pooling.init(opts.hiddenSize * 2, -1, mem);
+		_min_pooling.init(opts.hiddenSize * 2, -1, mem);
+		_concat.init(opts.hiddenSize * 3 * 2, -1, mem);
+		_output.setParam(&model.olayer_linear);
+		_output.init(opts.labelSize, -1, mem);
+	}
+
+
+public:
+	// some nodes may behave different during training and decode, for example, dropout
+	inline void forward(const Feature& feature, bool bTrain = false){
+		//first step: clear value
+		clearValue(bTrain); // compute is a must step for train, predict and cost computation
+
+		// second step: build graph
+		//forward
+		int words_num = feature.m_tweet_words.size();
+		if (words_num > max_sentence_length)
+			words_num = max_sentence_length;
+		for (int i = 0; i < words_num; i++) {
+			_word_inputs[i].forward(this, feature.m_tweet_words[i]);
+		}
+		_word_window.forward(this, getPNodes(_word_inputs, words_num));
+
+		_left_rnn.forward(this, getPNodes(_word_window._outputs, words_num));
+		_right_rnn.forward(this, getPNodes(_word_window._outputs, words_num));
+
+		for (int i = 0; i < words_num; i++) {
+			_rnn_concat[i].forward(this, &_left_rnn._hiddens[i], &_right_rnn._hiddens[i]);
+		}
+
+		_avg_pooling.forward(this, getPNodes(_rnn_concat, words_num));
+		_max_pooling.forward(this, getPNodes(_rnn_concat, words_num));
+		_min_pooling.forward(this, getPNodes(_rnn_concat, words_num));
+		_concat.forward(this, &_avg_pooling, &_max_pooling, &_min_pooling);
+		_output.forward(this, &_concat);
+	}
+};
+
+#endif /* SRC_ComputionGraph_H_ */
---
+++ src/NNBiRNNLabeler/model/ModelParams.h
@@ -0,0 +1,73 @@
+#ifndef SRC_ModelParams_H_
+#define SRC_ModelParams_H_
+#include "HyperParams.h"
+
+// Each model consists of two parts, building neural graph and defining output losses.
+class ModelParams{
+
+public:
+	Alphabet wordAlpha; // should be initialized outside
+	LookupTable words; // should be initialized outside
+	RNNParams left_rnn_params;
+	RNNParams right_rnn_params;
+	UniParams olayer_linear; // output
+public:
+	Alphabet labelAlpha; // should be initialized outside
+	SoftMaxLoss loss;
+
+
+public:
+	bool initial(HyperParams& opts, AlignedMemoryPool* mem = NULL){
+
+		// some model parameters should be initialized outside
+		if (words.nVSize <= 0 || labelAlpha.size() <= 0){
+			return false;
+		}
+		opts.wordDim = words.nDim;
+		opts.wordWindow = opts.wordContext * 2 + 1;
+		opts.windowOutput = opts.wordDim * opts.wordWindow;
+		opts.labelSize = labelAlpha.size();
+		left_rnn_params.initial(opts.hiddenSize, opts.windowOutput, mem);
+		right_rnn_params.initial(opts.hiddenSize, opts.windowOutput, mem);
+		opts.inputSize = opts.hiddenSize * 3 * 2;
+		olayer_linear.initial(opts.labelSize, opts.inputSize, false, mem);
+		return true;
+	}
+
+	bool TestInitial(HyperParams& opts, AlignedMemoryPool* mem = NULL){
+
+		// some model parameters should be initialized outside
+		if (words.nVSize <= 0 || labelAlpha.size() <= 0){
+			return false;
+		}
+		opts.wordDim = words.nDim;
+		opts.wordWindow = opts.wordContext * 2 + 1;
+		opts.windowOutput = opts.wordDim * opts.wordWindow;
+		opts.labelSize = labelAlpha.size();
+		opts.inputSize = opts.hiddenSize * 3 * 2;
+		return true;
+	}
+
+	void exportModelParams(ModelUpdate& ada){
+		words.exportAdaParams(ada);
+		left_rnn_params.exportAdaParams(ada);
+		right_rnn_params.exportAdaParams(ada);
+		olayer_linear.exportAdaParams(ada);
+	}
+
+
+	void exportCheckGradParams(CheckGrad& checkgrad){
+		checkgrad.add(&words.E, "words.E");
+		checkgrad.add(&(olayer_linear.W), "olayer_linear.W");
+	}
+
+	// will add it later
+	void saveModel(std::ofstream &os) const{
+	}
+
+	void loadModel(std::ifstream &is, AlignedMemoryPool* mem = NULL){
+	}
+
+};
+
+#endif /* SRC_ModelParams_H_ */
