abbbdd77e29b19d435d58e519e2a4e3d0cc3b8a0
YuNan <yunan.hlju@gmail.com>
2017-06-04 20:23:10 +0800

    char rnn

---
+++ src/NNLSTMCharLabeler/model/ModelParams.h
@@ -0,0 +1,86 @@
+#ifndef SRC_ModelParams_H_
+#define SRC_ModelParams_H_
+#include "HyperParams.h"
+
+// Each model consists of two parts, building neural graph and defining output losses.
+class ModelParams{
+
+public:
+	Alphabet wordAlpha; // should be initialized outside
+	LookupTable words; // should be initialized outside
+	Alphabet charAlpha; // should be initialized outside
+	LookupTable chars; // should be initialized outside
+	LSTMParams rnn_layer;
+	UniParams char_hidden_linear;
+	UniParams olayer_linear; // output
+public:
+	Alphabet labelAlpha; // should be initialized outside
+	SoftMaxLoss loss;
+
+
+public:
+	bool initial(HyperParams& opts, AlignedMemoryPool* mem = NULL){
+
+		// some model parameters should be initialized outside
+		if (words.nVSize <= 0 || labelAlpha.size() <= 0){
+			return false;
+		}
+		opts.wordDim = words.nDim;
+		opts.wordWindow = opts.wordContext * 2 + 1;
+		opts.wordWindowOutput = opts.wordDim * opts.wordWindow;
+
+		opts.charDim = chars.nDim;
+		opts.charWindow = opts.charContext * 2 + 1;
+		opts.charWindowOutput = opts.charDim * opts.charWindow;
+
+		opts.labelSize = labelAlpha.size();
+		rnn_layer.initial(opts.rnnHiddenSize, opts.wordWindowOutput, mem);
+		char_hidden_linear.initial(opts.charHiddenSize, opts.charWindowOutput, true, mem);
+		opts.inputSize = opts.rnnHiddenSize * 3 + opts.charHiddenSize * 3;
+		olayer_linear.initial(opts.labelSize, opts.inputSize, false, mem);
+		return true;
+	}
+
+	bool TestInitial(HyperParams& opts, AlignedMemoryPool* mem = NULL){
+
+		// some model parameters should be initialized outside
+		if (words.nVSize <= 0 || labelAlpha.size() <= 0){
+			return false;
+		}
+		opts.wordDim = words.nDim;
+		opts.wordWindow = opts.wordContext * 2 + 1;
+		opts.wordWindowOutput = opts.wordDim * opts.wordWindow;
+
+		opts.charDim = chars.nDim;
+		opts.charWindow = opts.charContext * 2 + 1;
+		opts.charWindowOutput = opts.charDim * opts.charWindow;
+
+		opts.labelSize = labelAlpha.size();
+		opts.inputSize = opts.rnnHiddenSize * 3 + opts.charHiddenSize * 3;
+		return true;
+	}
+
+	void exportModelParams(ModelUpdate& ada){
+		words.exportAdaParams(ada);
+		chars.exportAdaParams(ada);
+		rnn_layer.exportAdaParams(ada);
+		char_hidden_linear.exportAdaParams(ada);
+		olayer_linear.exportAdaParams(ada);
+	}
+
+
+	void exportCheckGradParams(CheckGrad& checkgrad){
+		checkgrad.add(&words.E, "words.E");
+		checkgrad.add(&(olayer_linear.W), "olayer_linear.W");
+	}
+
+	// will add it later
+	void saveModel(std::ofstream &os) const{
+	}
+
+	void loadModel(std::ifstream &is, AlignedMemoryPool* mem = NULL){
+	}
+
+};
+
+#endif /* SRC_ModelParams_H_ */
---
+++ src/NNRNNCharLabeler/model/ComputionGraph.h
@@ -0,0 +1,148 @@
+#ifndef SRC_ComputionGraph_H_
+#define SRC_ComputionGraph_H_
+
+#include "ModelParams.h"
+
+
+// Each model consists of two parts, building neural graph and defining output losses.
+struct ComputionGraph : Graph{
+public:
+	const static int max_sentence_length = 2048;
+	const static int max_char_length = 4096;
+
+public:
+	// node instances
+	vector<LookupNode> _word_inputs;
+	WindowBuilder _word_window;
+	RNNBuilder _rnn;
+
+	AvgPoolNode _avg_word_pooling;
+	MaxPoolNode _max_word_pooling;
+	MinPoolNode _min_word_pooling;
+
+	ConcatNode _word_pooling_concat;
+
+	vector<LookupNode> _char_inputs;
+	WindowBuilder _char_window;
+	vector<UniNode> _char_hidden;
+
+	AvgPoolNode _avg_char_pooling;
+	MaxPoolNode _max_char_pooling;
+	MinPoolNode _min_char_pooling;
+	ConcatNode _char_pooling_concat;
+
+	ConcatNode _concat;
+	LinearNode _output;
+public:
+	ComputionGraph() : Graph(){
+	}
+
+	~ComputionGraph(){
+		clear();
+	}
+
+public:
+	//allocate enough nodes 
+	inline void createNodes(int sent_length, int char_length){
+		_word_inputs.resize(sent_length);
+		_word_window.resize(sent_length);
+		_rnn.resize(sent_length);
+		_avg_word_pooling.setParam(sent_length);
+		_max_word_pooling.setParam(sent_length);
+		_min_word_pooling.setParam(sent_length);
+
+		_char_inputs.resize(char_length);
+		_char_window.resize(char_length);
+		_char_hidden.resize(char_length);
+		_avg_char_pooling.setParam(char_length);
+		_max_char_pooling.setParam(char_length);
+		_min_char_pooling.setParam(char_length);
+	}
+
+	inline void clear(){
+		Graph::clear();
+		_word_inputs.clear();
+		_word_window.clear();
+		_rnn.clear();
+		_char_inputs.clear();
+		_char_window.clear();
+		_char_hidden.clear();
+	}
+
+public:
+	inline void initial(ModelParams& model, HyperParams& opts, AlignedMemoryPool* mem = NULL){
+		for (int idx = 0; idx < _word_inputs.size(); idx++) {
+			_word_inputs[idx].setParam(&model.words);
+			_word_inputs[idx].init(opts.wordDim, opts.dropProb, mem);
+		}
+
+		_word_window.init(opts.wordDim, opts.wordContext, mem);
+		_rnn.init(&model.rnn_layer, opts.dropProb, true, mem);
+		_avg_word_pooling.init(opts.rnnHiddenSize, -1, mem);
+		_max_word_pooling.init(opts.rnnHiddenSize, -1, mem);
+		_min_word_pooling.init(opts.rnnHiddenSize, -1, mem);
+		_word_pooling_concat.init(opts.rnnHiddenSize * 3, -1, mem);
+
+		for (int idx = 0; idx < _char_inputs.size(); idx++) {
+			_char_inputs[idx].setParam(&model.chars);
+			_char_inputs[idx].init(opts.charDim, opts.dropProb, mem);
+			_char_hidden[idx].setParam(&model.char_hidden_linear);
+			_char_hidden[idx].init(opts.charHiddenSize, opts.dropProb, mem);
+		}
+
+		_char_window.init(opts.charDim, opts.charContext, mem);
+		_avg_char_pooling.init(opts.charHiddenSize, -1, mem);
+		_max_char_pooling.init(opts.charHiddenSize, -1, mem);
+		_min_char_pooling.init(opts.charHiddenSize, -1, mem);
+		_char_pooling_concat.init(opts.charHiddenSize * 3, -1, mem);
+
+		_concat.init(opts.charHiddenSize * 3 + opts.rnnHiddenSize * 3, -1, mem);
+		_output.setParam(&model.olayer_linear);
+		_output.init(opts.labelSize, -1, mem);
+	}
+
+
+public:
+	// some nodes may behave different during training and decode, for example, dropout
+	inline void forward(const Feature& feature, bool bTrain = false){
+		//first step: clear value
+		clearValue(bTrain); // compute is a must step for train, predict and cost computation
+
+		// second step: build graph
+		//forward
+		int words_num = feature.m_tweet_words.size();
+		if (words_num > max_sentence_length)
+			words_num = max_sentence_length;
+		for (int i = 0; i < words_num; i++) {
+			_word_inputs[i].forward(this, feature.m_tweet_words[i]);
+		}
+		_word_window.forward(this, getPNodes(_word_inputs, words_num));
+
+		_rnn.forward(this, getPNodes(_word_window._outputs, words_num));
+		_avg_word_pooling.forward(this, getPNodes(_rnn._output, words_num));
+		_max_word_pooling.forward(this, getPNodes(_rnn._output, words_num));
+		_min_word_pooling.forward(this, getPNodes(_rnn._output, words_num));
+		_word_pooling_concat.forward(this, &_avg_word_pooling, &_max_word_pooling, &_min_word_pooling);
+
+		int chars_num = feature.m_chars.size();
+		if (chars_num > max_char_length)
+			chars_num = max_char_length;
+		for (int i = 0; i < chars_num; i++) {
+			_char_inputs[i].forward(this, feature.m_chars[i]);
+		}
+		_char_window.forward(this, getPNodes(_char_inputs, chars_num));
+
+		for (int i = 0; i < chars_num; i++) {
+			_char_hidden[i].forward(this, &_char_window._outputs[i]);
+		}
+		_avg_char_pooling.forward(this, getPNodes(_char_hidden, chars_num));
+		_max_char_pooling.forward(this, getPNodes(_char_hidden, chars_num));
+		_min_char_pooling.forward(this, getPNodes(_char_hidden, chars_num));
+		_char_pooling_concat.forward(this, &_avg_char_pooling, &_max_char_pooling, &_min_char_pooling);
+
+		_concat.forward(this, &_word_pooling_concat, &_char_pooling_concat);
+		_output.forward(this, &_concat);
+	}
+};
+
+#endif /* SRC_ComputionGraph_H_ */
